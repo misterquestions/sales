97113facca62eaa861209e65e853585e56e64b3e
Fernando Lopez <misterquestions.mx@gmail.com>
2021-10-26 23:28:23 -0500

    feat: update sales model and register in admin

--- api/apps/sales/models.py
+++ api/apps/sales/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from api.apps.teams.models import Team
+
+
+class User(AbstractUser):
+    team = models.ForeignKey(Team, null=True, on_delete=models.SET_NULL)
+
+
+class Sale(models.Model):
+    created_at = models.DateTimeField(null=False)
+    amount = models.DecimalField(max_digits=6, decimal_places=2, null=False)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'Sale {self.id}'
